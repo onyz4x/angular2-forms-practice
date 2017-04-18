4ee4e0ecee4c83d3e744a9d0ad50fc580c972748
troy <troy.hu@okchem.com>
2017-04-18 15:49:25 +0800

    angular表单的练习

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+
+export class HeroFormComponent implements OnInit {
+  powers = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Dr IQ', this.powers[1], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
